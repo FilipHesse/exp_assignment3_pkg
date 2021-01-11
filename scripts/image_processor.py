44d25223728fe748be94a150b1d913a2985eae23
Filip Hesse <Filip_Hesse@yahoo.de>
2021-01-11 15:54:52 +0100

    Image processor detects all balls

--- scripts/image_processor.py
+++ scripts/image_processor.py
@@ -16,6 +16,14 @@ from std_msgs.msg import Bool
 from std_msgs.msg import Int64
 from exp_assignment3_pkg.msg import BallCenterRadius
 
+class ColoredBall:
+    def __init__(self, color_str, lower_bound, upper_bound):
+        self.color_str = color_str
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.radius = 0
+        self.center = [0,0]
+        self.visible= False
 
 
 class ImageProcessor:
@@ -38,7 +46,33 @@ class ImageProcessor:
 
         self.bridge = CvBridge()
         
-    
+    def comp_ball_center_radius(self, hsv_img, ball):
+        mask = cv2.inRange(hsv_img, ball.lower_bound, ball.upper_bound)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        #cv2.imshow('mask', mask)
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        ball.center = [0, 0]
+        ball.radius = 0
+        ball.visible = False
+
+                # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), ball.radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            ball.center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            # only proceed if the radius meets a minimum size
+            if ball.radius > 10:
+                ball.visible = True
+
+
     def callback_raw_image(self, msg):
         """ Callback: new image came is -> Process it
 
@@ -49,49 +83,34 @@ class ImageProcessor:
         """
         self.image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
 
-        greenLower = (50, 50, 20)
-        greenUpper = (70, 255, 255)
-
+        colored_balls = [   ColoredBall('green',(50, 50, 20),(70, 255, 255)),
+                            ColoredBall('yellow',(25, 50, 20),(32, 255, 255)),
+                            ColoredBall('red',(0, 50, 20),(12, 255, 255)),
+                            ColoredBall('blue',(105, 50, 20),(135, 255, 255)),
+                            ColoredBall('pink',(143, 50, 20),(160, 255, 255)),
+                            ColoredBall('black',(0, 0, 0),(179, 255, 10))]
+        
         blurred = cv2.GaussianBlur(self.image, (11, 11), 0)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, greenLower, greenUpper)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        #cv2.imshow('mask', mask)
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        center = [0, 0]
-        radius = 0
-        visible = False
 
-                # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        for ball in colored_balls:
+            self.comp_ball_center_radius(hsv, ball)
 
-            # only proceed if the radius meets a minimum size
-            if radius > 10:
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                visible = True
-                cv2.circle(self.image, (int(x), int(y)), int(radius),
-                           (0, 255, 255), 2)
-                cv2.circle(self.image, center, 5, (0, 0, 255), -1)
+        biggest_ball = max(colored_balls, key=lambda x : x.radius)
+        if biggest_ball.visible:
+            cv2.circle(self.image, biggest_ball.center, int(biggest_ball.radius),
+                        (0, 255, 255), 2)
+            cv2.circle(self.image, biggest_ball.center, 5, (0, 0, 255), -1)
+
 
         self.pub_img.publish((self.bridge.cv2_to_imgmsg(self.image, 'bgr8')))
-        self.pub_vis.publish(visible)
+        self.pub_vis.publish(biggest_ball.visible)
 
         cr = BallCenterRadius()
-        cr.visible = Bool(visible)
-        cr.center_x = Int64(center[0])
-        cr.center_y = Int64(center[1])
-        cr.radius = Int64(int(radius))
+        cr.visible = Bool(biggest_ball.visible)
+        cr.center_x = Int64(biggest_ball.center[0])
+        cr.center_y = Int64(biggest_ball.center[1])
+        cr.radius = Int64(int(biggest_ball.radius))
         self.pub_cr.publish(cr)
 
 
